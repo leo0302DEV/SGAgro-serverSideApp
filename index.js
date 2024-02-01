ec8a04675ceff14ac4f7d662618d35f7fad0ea76
Leonardo Kramer Nadal <leokn0102@gmail.com>
2024-01-31 21:26:35 -0300

    Adicionando configurações de deploy ao projeto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./src/app.js";
 
-const PORT = 3000;
+const PORT = process.env.APP_PORT;
 
 app.listen(PORT, () => {
     console.log("Server runing on http://localhost:3000");
