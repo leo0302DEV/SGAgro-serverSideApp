75b8aa6577652c4a1492dfbec41a777470dd1f7d
Leonardo Kramer Nadal <leokn0102@gmail.com>
2024-02-07 22:40:52 -0300

    Adicionando cors ao servidor

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,17 @@
 import express from "express";
 import animals from "./animalRouts.js";
+import cors from "cors";
+
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}
 
 const routes = (app) => {
     app.route("/").get((req, res) => {
         res.status(200).send("Rota de teste");
     });
-    app.use(express.json(), animals);
+    app.use(express.json(), cors(corsOptions), animals);
 };
 
 export default routes;
